a5da8062eac2793e584a9e15d500c56496d47d0d
Ankit Keshari <GS-1544@GSLAB.COM>
2018-01-23 22:24:13 +0530

    Second Deployment

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
 angular.module('ngSmac',['ui.bootstrap']);
-
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname,'/app'));
-app.listen(process.env.PORT || 3000);
